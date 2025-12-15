ee23ad81ad86bd9416cb7bbc467afdcf2481a69c
olivblvck <witkowska.oliwia@vp.pl>
2025-12-15 22:40:22 +0100

    Initial Commit

---
+++ Safari-Phishing-DetectorTests/Safari_Phishing_DetectorTests.swift
@@ -0,0 +1,17 @@
+//
+//  Safari_Phishing_DetectorTests.swift
+//  Safari-Phishing-DetectorTests
+//
+//  Created by Oliwia on 15/12/2025.
+//
+
+import Testing
+@testable import Safari_Phishing_Detector
+
+struct Safari_Phishing_DetectorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
